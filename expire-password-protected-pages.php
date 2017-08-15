933b576ab700411f705177f6ffa46cd946aa0012
Troy Glancy <troy.glancy@gmail.com>
2017-08-15 13:46:00 -0400

    Update expire-password-protected-pages.php

--- expire-password-protected-pages.php
+++ expire-password-protected-pages.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Expires Password Protected Pages
 Plugin URI: https://github.com/troyglancy/Expire-Password-Protected-Pages
-Description: This plugin will require vistior to type in password each time when visiting a password protected page. This will prevent the page from being accessable if someone logins via a public computer. 
+Description: This plugin will require vistior to type in password each time when visiting a password protected page. This also will prevent the page from being accessable if someone logins via a public computer. 
 Version: 0.1.0
 Author: Troy Glancy 
 Author URI: http://troyglancy.com
