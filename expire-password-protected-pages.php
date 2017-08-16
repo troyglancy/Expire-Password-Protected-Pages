7ba166282a6d48063c8de2534939b74838e2d511
Troy Glancy <troy.glancy@gmail.com>
2017-08-16 10:55:52 -0400

    Update expire-password-protected-pages.php

--- expire-password-protected-pages.php
+++ expire-password-protected-pages.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Expire Password Protected Pages
 Plugin URI: https://wordpress.org/plugins/expire-password-protected-pages/
-Description: This plugin will require visitors to type in the password each time they are visiting a password protected page. This also will prevent the page from being accessable if someone types in the password on a public computer.  By default WordPress password protected pages sessions will last 10 days. Originally came up with this idea when a client did not want a password protected pages accessable after the browser was closed.. While you could add the code to your function.php within your theme I did not want the code to get overwritten when doing a theme update. Using this plugin avoids the need for updating the function.php file. 
+Description: This plugin will require visitors to type in the password each time they are visiting a password protected page. This also will prevent the page from being accessable if someone types in the password on a public computer.  By default WordPress password protected pages sessions will last 10 days. Originally came up with this idea when a client did not want a password protected pages accessable after the browser was closed. While you could add the code to your function.php within your theme I did not want the code to get overwritten when doing a theme update. Using this plugin avoids the need for updating the function.php file. 
 Version: 0.1.0
 Author: Troy Glancy 
 Author URI: http://troyglancy.com
